34a0333250b2cd6fc207678bb39281acbbb4ef66
dj <1278349695@qq.com>
2018-07-12 20:31:53 +0800

    开始学习django，利用python写服务器

---
+++ HelloWorld/urls.py
@@ -0,0 +1,13 @@
+from django.conf.urls import url
+from django.contrib import admin
+from . import view, testdb, search
+
+urlpatterns = [
+	url(r'^$', view.hello),
+    url(r'^testdb$', testdb.insertData),
+    url(r'^getUser$', testdb.getData),
+    url(r'^search-form$', search.search_form),
+    url(r'^search$', search.search),
+    url(r'^search-post$', search.search_post),
+    url(r'^admin/', admin.site.urls),
+]
